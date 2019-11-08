2f58ff48cd202cd106b7f358ffbe8c6380cd28ce
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-11-08 11:33:06 -0500

    created initial State and set up a simple reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: null
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        default: 
+        return state;
+    }
+}
+
+export default reducer;
