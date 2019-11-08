3e0b2b15ee0a132620e953d27166c4510ddb7ff5
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-11-08 12:12:52 -0500

    changed the smurf list to use useDispatch and useEffect as it makes more sense to me and writes as cleaner code

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchSmurfs } from '../actions';
 import Smurf from './Smurf';
 
-function SmurfList(props) {
-    console.log('smurf list', props);
-    return (
-        <>
-        {props.isFetching && <div>loading...</div>}
-        {props.error && <div>{props.error.message}</div>}
-        {props.smurfs.map(smurf => <Smurf key={smurf.id} data={smurf} /> )};
-        </>
-    );
+
+
+const SmurfList = () => {
+    const dispatch = useDispatch();
+    const smurfs = useSelector(state => state.smurfs);
+
+
+    useEffect(() => {
+        dispatch(fetchSmurfs());
+    }, []);
+
+    const renderSmurfs = () => {
+        return smurfs.map(smurf => <Smurf key={smurf.id} data={smurf} />);
+    };
+
+    return <div>{smurfs ? renderSmurfs() : 'Here come the Smurfs...'}</div>
 };
 
+export default SmurfList;
+
+// function SmurfList(props) {
+//     console.log('smurf list', props);
+//     return (
+//         <>
+//         {props.isFetching && <div>loading...</div>}
+//         {props.error && <div>{props.error.message}</div>}
+//         {props.smurfs.map(smurf => <Smurf key={smurf.id} data={smurf} /> )};
+//         </>
+//     );
+// };
+
+// const mapDispatchToProps = {
+//     fetchSmurfs
+// }
+
+// export default connect(state => {
+//     console.log('mSTP.state', state);
+//     return state;
+// }, 
+// mapDispatchToProps)(SmurfList);
